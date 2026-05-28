9189cc901cb0310578543b6b9e210a0e1b2ca3a3
andy6192464 <andy6192464@gmail.com>
2026-05-27 20:19:49 -0500

    Update Vehiculo.java

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -8,6 +8,33 @@ public class Vehiculo {
 
     public vehiculo(){
     }
-    public vehiculo(String tipo, double consumoGasolina, double valorPasaje, int capacidadMax, double peaje){
+    //parametros
+    public vehiculo (String tipo, double consumoGasolina, double valorPasaje, int capacidadMax, double peaje){
+
+        this.tipo = tipo;
+        this.consumoGasolina = consumoGasolina;
+        this.valorPasaje = valorPasaje;
+        this.capacidadMax = capacidadMax;
+        this.peaje = peaje;
+    }
+// calculo gasto gasolina
+    public double gastoGasolina (Rutas ruta, int pasajeros, boolean esAutopista) {
         
+        double gasto = ruta.getDistancia() * consumoGasolina;
+
+        if (esAutopista){
+            gasto += peaje;
+        }
+        return gastos;
+    }
+    //calculo ingreos
+    public double calcularIngresos (int pasajeros) {
+        return pasajeros * valorPasaje;
+    }
+    //calculo rentabilidad
+    public double calcularRentabilidad (double ingreos, double gastos) {
+        return ingresos - gastos;
+    }
+    
+    
 }
