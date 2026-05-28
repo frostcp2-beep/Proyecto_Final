53e493e69f0c9c139ec11e1162c6837cd63c08f4
andy6192464 <andy6192464@gmail.com>
2026-05-28 11:54:41 -0500

    Update Ruta.java

--- src/Ruta.java
+++ src/Ruta.java
@@ -1,13 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author xdpor
- */
 public class Ruta {
-    
+    private String nombreRuta;
+    private int valorPeaje;
+    private int marcha;
+
+    public double distancia;
+    public boolean autopista;
+
+    public Rutas (String nombreRuta, int valorPeaje, int marcha, double distancia, boolean autopista) {
+        this.nombreRuta = nombreRuta;
+        this.valorPeaje = marcha;
+        this.distancia = distancia;
+        this.autopista = autopista;
+    }
+    public double obDistancia(){
+        return distancia;
+    }
+    public boolean isAutopista(){
+        return autopista;
+    }
 }
