3d7db53c86ed344927f359831461873383ab3d7a
andy6192464 <andy6192464@gmail.com>
2026-05-28 17:07:05 -0500

    Update Empresa.java

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,13 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author xdpor
- */
 public class Empresa {
+    private String nombre;
+    private Vehiculo vehiculo;
+
+    public Empresa(
+            String nombre,
+            Vehiculo vehiculo) {
+
+        this.nombre = nombre;
+        this.vehiculo = vehiculo;
+    }
+
+    public void matrizRentabilidad() {
+
+        System.out.println(
+                "Matriz");
+    }
+
+    public void simulacionSemana() {
+
+        System.out.println(
+                "Simulacion Semanal");
+    }
+
+    public void imprimirPromedio() {
+
+        System.out.println(
+                "Promedio");
+    }
     
 }
