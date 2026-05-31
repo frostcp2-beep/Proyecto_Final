4f23acab50bca8f48b4d698d3f97ba4a30495a17
andy6192464 <andy6192464@gmail.com>
2026-05-30 20:15:58 -0500

    mod3

--- src/Taxi.java
+++ src/Taxi.java
@@ -1,4 +1,23 @@
 
-public class Taxi extends Vehiculo{
-    
+public class Taxi extends Vehiculo {
+
+    // Constructor
+    public Taxi() {
+
+        super("Taxi",50,25000, 4, 15500); //el "SUPER" le dara los atributos a la plantilla "THIS" de la superclase
+    }
+
+    // Recargo pasajeros
+    @Override
+    public double calcularRecargoPasajeros(int pasajeros) {//aqui el override le da el parametro que va a utilizar este tipo
+
+        return pasajeros * 0.05;
+    }
+
+    // Recargo subida
+    @Override
+    public double calcularRecargoSubida(double gastoBase) {//aqui el override le da el parametro que va a utilizar este tipo
+
+        return gastoBase * 0.10;
+    }
 }
--- src/Vans.java
+++ src/Vans.java
@@ -1,13 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+public class Vans extends Vehiculo {
 
-/**
- *
- * @author xdpor
- */
-public class Vans extends Vehiculo{
-    
+    // Constructor
+    public Vans() {
+
+        super( "Vans",40, 17000,10,19500);//el "SUPER" le dara los atributos a la plantilla "THIS" de la superclase
+    }
+
+    // Recargo pasajeros
+    @Override
+    public double calcularRecargoPasajeros(int pasajeros) {//aqui el override le da el parametro que va a utilizar este tipo
+
+        return pasajeros * 0.08;
+    }
+
+    // Recargo subida
+    @Override
+    public double calcularRecargoSubida( double gastoBase) {//aqui el override le da el parametro que va a utilizar este tipo
+
+        return gastoBase * 0.15;
+    }
 }
